7d349265ffa91f7296232bc32c24648337cbc25c
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2023-12-05 21:41:00 +0100

    add solution to puzzles from day05

---
+++ day05/src/main.rs
@@ -0,0 +1,94 @@
+use rayon::prelude::*;
+
+#[derive(Debug)]
+struct Map {
+    destination_start: u64,
+    source_start: u64,
+    length: u64,
+}
+
+impl Map {
+    fn affects(&self, number: u64) -> bool {
+        (self.source_start..self.source_start + self.length).contains(&number)
+    }
+
+    fn map(&self, number: u64) -> u64 {
+        let shift = number - self.source_start;
+        self.destination_start + shift
+    }
+}
+
+fn part_1(almanac: &[Vec<Map>], seeds: Vec<u64>) -> u64 {
+    let mut global_min = u64::MAX;
+    for mut seed in seeds {
+        for maps in almanac.iter() {
+            for map in maps {
+                if map.affects(seed) {
+                    seed = map.map(seed);
+                    break;
+                }
+            }
+        }
+        global_min = global_min.min(seed);
+    }
+    global_min
+}
+
+fn part_2(almanac: &[Vec<Map>], seeds: Vec<u64>) -> u64 {
+    seeds
+        .par_chunks(2)
+        .map(|seed_range| {
+            let mut chunk_min = u64::MAX;
+            let start = seed_range[0];
+            let length = seed_range[1];
+
+            for mut seed in start..start + length {
+                for maps in almanac.iter() {
+                    for map in maps {
+                        if map.affects(seed) {
+                            seed = map.map(seed);
+                            break;
+                        }
+                    }
+                }
+                chunk_min = chunk_min.min(seed);
+            }
+
+            chunk_min
+        })
+        .min()
+        .unwrap()
+}
+
+fn main() {
+    let input = std::fs::read_to_string("input").unwrap();
+    let mut parts = input.split("\n\n");
+
+    let seeds = parts.next().unwrap();
+    let (_, seeds) = seeds.split_once(": ").unwrap();
+    let seeds = seeds
+        .split_whitespace()
+        .map(|n| n.parse::<u64>().unwrap())
+        .collect::<Vec<_>>();
+
+    let mut almanac = vec![];
+    for part in parts {
+        let maps = part
+            .lines()
+            .skip(1)
+            .map(|line| {
+                let mut ns = line.split_whitespace();
+                Map {
+                    destination_start: ns.next().unwrap().parse().unwrap(),
+                    source_start: ns.next().unwrap().parse().unwrap(),
+                    length: ns.next().unwrap().parse().unwrap(),
+                }
+            })
+            .collect::<Vec<_>>();
+
+        almanac.push(maps);
+    }
+
+    assert_eq!(part_1(&almanac, seeds.clone()), 88151870);
+    assert_eq!(part_2(&almanac, seeds.clone()), 2008785);
+}
