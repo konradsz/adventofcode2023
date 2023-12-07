d10c627ebf6a54e7029d1172259babe3c1b83b90
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2023-12-07 23:13:09 +0100

    little cleanup of first solution

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -91,20 +91,10 @@ impl Hand {
         }
         match chars.len() {
             1 => Type::FiveOfAKind,
-            2 => {
-                if chars.values().any(|v| *v == 4) {
-                    Type::FourOfAKind
-                } else {
-                    Type::FullHouse
-                }
-            }
-            3 => {
-                if chars.values().any(|v| *v == 3) {
-                    Type::ThreeOfAKind
-                } else {
-                    Type::TwoPair
-                }
-            }
+            2 if chars.values().any(|v| *v == 4) => Type::FourOfAKind,
+            2 => Type::FullHouse,
+            3 if chars.values().any(|v| *v == 3) => Type::ThreeOfAKind,
+            3 => Type::TwoPair,
             4 => Type::OnePair,
             5 => Type::HighCard,
             _ => panic!("whoops"),
@@ -127,10 +117,11 @@ fn main() {
 
     let len = hands.len();
 
-    let mut sum = 0;
-    for (idx, HandAndBid(_, bid)) in hands.into_iter().enumerate() {
-        sum += (len - idx) as u64 * bid;
-    }
+    let sum: u64 = hands
+        .into_iter()
+        .enumerate()
+        .map(|(idx, HandAndBid(_, bid))| (len - idx) as u64 * bid)
+        .sum();
 
-    println!("{sum}");
+    assert_eq!(sum, 248812215);
 }
