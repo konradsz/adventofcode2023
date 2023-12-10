4862db374758fb9e870ed6378f4254303aeb4bb1
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2023-12-10 23:39:00 +0100

    solve part2 using raycasting

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::VecDeque;
-
 #[derive(Clone, Copy, PartialEq, Eq)]
 enum Tile {
     Vertical,         // |
@@ -10,8 +8,6 @@ enum Tile {
     SouthEast,        // F
     Ground,           // .
     StartingPosition, // S
-    Inner,
-    Outter,
 }
 
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -97,7 +93,7 @@ fn main() {
         let (starting_x, starting_y) = starting_position;
         let new_x = starting_x as i32 + dx;
         let new_y = starting_y as i32 + dy;
-        if new_x > map[0].len() as i32 || new_x < 0 || new_y > map.len() as i32 || new_x < 0 {
+        if new_x >= map[0].len() as i32 || new_x < 0 || new_y >= map.len() as i32 || new_y < 0 {
             continue;
         }
 
@@ -142,115 +138,26 @@ fn main() {
         }
     }
 
-    // assert_eq!(loop_positions.len() / 2, 6768);
-
-    flood_fill(&mut map, &loop_positions);
-
-    let mut count = 0;
-    for (y, row) in map.iter().enumerate() {
-        for (x, t) in row.iter().enumerate() {
-            if t == &Tile::Inner {
-                count += 1;
-            }
-        }
-    }
-
-    for (y, row) in map.iter().enumerate() {
-        for (x, t) in row.iter().enumerate() {
-            if loop_positions.contains(&(x, y)) {
-                print!("@");
-                continue;
-            }
-            let ch = match t {
-                Tile::Vertical => '|',
-                Tile::Horizontal => '-',
-                Tile::NorthEast => 'L',
-                Tile::NorthWest => 'J',
-                Tile::SouthWest => '7',
-                Tile::SouthEast => 'F',
-                Tile::Ground => '.',
-                Tile::StartingPosition => 'S',
-                Tile::Inner => 'I',
-                Tile::Outter => 'O',
-            };
-            print!("{}", ch);
-        }
-        println!();
-    }
-
-    println!("{count}");
+    assert_eq!(loop_positions.len() / 2, 6768); // Part 1
+    assert_eq!(ray_cast(&map, &loop_positions), 351); // Part 2
 }
 
-fn flood_fill(map: &mut Vec<Vec<Tile>>, loop_positions: &[(usize, usize)]) {
-    loop {
-        let mut ground_pos = None;
-        for (y, row) in map.iter().enumerate() {
-            for (x, t) in row.iter().enumerate() {
-                if t == &Tile::Ground {
-                    ground_pos = Some((x, y));
+fn ray_cast(map: &[Vec<Tile>], loop_positions: &[(usize, usize)]) -> usize {
+    let mut count = 0;
+    for (y, row) in map.iter().enumerate() {
+        let mut hits = 0;
+        for (x, t) in row.iter().enumerate() {
+            if loop_positions.contains(&(x, y)) {
+                if t == &Tile::NorthEast || t == &Tile::NorthWest || t == &Tile::Vertical {
+                    hits += 1;
+                }
+            } else {
+                if hits % 2 != 0 {
+                    count += 1;
                 }
             }
         }
-
-        let mut to_fill = VecDeque::new();
-        if let Some(gp) = ground_pos {
-            to_fill.push_back(gp);
-        } else {
-            break;
-        }
-
-        let mut current_cluster = vec![];
-        let mut inner = true;
-        // while !to_fill.is_empty() {
-        while let Some(position) = to_fill.pop_front() {
-            if current_cluster.contains(&position) {
-                continue;
-            }
-
-            current_cluster.push(position);
-            for (dx, dy) in DIRS {
-                let new_x = position.0 as i32 + dx;
-                let new_y = position.1 as i32 + dy;
-                if new_y < 0
-                    || new_y >= map.len() as i32
-                    || new_x < 0
-                    || new_x >= map[0].len() as i32
-                {
-                    inner = false;
-                    continue;
-                }
-
-                if map[new_y as usize][new_x as usize] == Tile::Ground {
-                    to_fill.push_back((new_x as usize, new_y as usize));
-                } else {
-                    if !loop_positions.contains(&(new_x as usize, new_y as usize)) {
-                        inner = false;
-                    }
-                }
-            }
-        }
-
-        for (x, y) in current_cluster {
-            map[y][x] = if inner { Tile::Inner } else { Tile::Outter };
-        }
-
-        // for (y, row) in map.iter().enumerate() {
-        //     for (x, t) in row.iter().enumerate() {
-        //         let ch = match t {
-        //             Tile::Vertical => '|',
-        //             Tile::Horizontal => '-',
-        //             Tile::NorthEast => 'L',
-        //             Tile::NorthWest => 'J',
-        //             Tile::SouthWest => '7',
-        //             Tile::SouthEast => 'F',
-        //             Tile::Ground => '.',
-        //             Tile::StartingPosition => 'S',
-        //             Tile::Inner => 'I',
-        //             Tile::Outter => 'O',
-        //         };
-        //         print!("{}", ch);
-        //     }
-        //     println!();
-        // }
     }
+
+    count
 }
