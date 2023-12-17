1afdd5d6fb436145d0fe04e5a1f5c9baa2f74a0f
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2023-12-17 21:48:28 +0100

    rewrite solution to solve both puzzles

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,82 +1,74 @@
-fn is_valid2(record: &[u8], groups: &[u32]) -> bool {
-    let mut damaged_groups = vec![];
-    let mut current_len = 0;
-    for condition in record {
-        if *condition == b'?' {
-            break;
-        }
+use std::collections::HashMap;
 
-        if *condition == b'#' {
-            current_len += 1;
-        } else {
-            if current_len > 0 {
-                damaged_groups.push(current_len);
-                current_len = 0;
-            }
-        }
-    }
-    if current_len > 0 {
-        damaged_groups.push(current_len);
+fn dfs(
+    cache: &mut HashMap<(usize, usize, u32), u64>,
+    record: &[u8],
+    groups: &[u32],
+    ptr: usize,
+    curr_group: usize,
+    curr_size: u32,
+) -> u64 {
+    if let Some(v) = cache.get(&(ptr, curr_group, curr_size)) {
+        return *v;
     }
 
-    damaged_groups == groups
-}
-
-fn is_partially_valid(record: &[u8], groups: &[u32]) -> bool {
-    let mut damaged_groups = vec![];
-    let mut current_len = 0;
-    for condition in record {
-        if *condition == b'?' {
-            break;
+    if ptr >= record.len() {
+        if curr_group >= groups.len() {
+            return 1;
         }
 
-        if *condition == b'#' {
-            current_len += 1;
-        } else {
-            if current_len > 0 {
-                damaged_groups.push(current_len);
-                current_len = 0;
-            }
+        if curr_group == groups.len() - 1 && groups[curr_group] == curr_size {
+            return 1;
         }
-    }
-    if current_len > 0 {
-        damaged_groups.push(current_len);
-    }
-
-    if damaged_groups.len() > groups.len() {
-        return false;
-    } else {
-        if damaged_groups.iter().zip(groups.iter()).any(|(a, b)| a > b) {
-            return false;
-        }
-    }
-
-    true
-}
-
-fn count_arrangements(input: &[u8], groups: &[u32]) -> u32 {
-    if !is_partially_valid(input, groups) {
         return 0;
     }
 
-    if !input.contains(&b'?') {
-        if is_valid2(input, groups) {
-            return 1;
-        } else {
-            return 0;
+    match record[ptr] {
+        b'.' => {
+            if curr_size == 0 {
+                return dfs(cache, record, groups, ptr + 1, curr_group, curr_size);
+            }
+
+            if curr_group >= groups.len() || curr_size != groups[curr_group] {
+                return 0;
+            }
+
+            dfs(cache, record, groups, ptr + 1, curr_group + 1, 0)
         }
+        b'#' => {
+            if curr_group >= groups.len() || groups[curr_group] < curr_size + 1 {
+                0
+            } else {
+                dfs(cache, record, groups, ptr + 1, curr_group, curr_size + 1)
+            }
+        }
+        b'?' => {
+            let mut possibilities = 0;
+
+            if curr_size == 0 {
+                let p = dfs(cache, record, groups, ptr + 1, curr_group, curr_size);
+                cache.insert((ptr + 1, curr_group, curr_size), p);
+                possibilities += p;
+            }
+
+            if curr_group < groups.len() && curr_size < groups[curr_group] {
+                let p = dfs(cache, record, groups, ptr + 1, curr_group, curr_size + 1);
+                cache.insert((ptr + 1, curr_group, curr_size + 1), p);
+                possibilities += p;
+            }
+
+            if curr_group < groups.len() && curr_size == groups[curr_group] {
+                let p = dfs(cache, record, groups, ptr + 1, curr_group + 1, 0);
+                cache.insert((ptr + 1, curr_group + 1, 0), p);
+                possibilities += p;
+            }
+            possibilities
+        }
+        _ => panic!("incorrect input"),
     }
-
-    let unknown = input.iter().position(|c| *c == b'?').unwrap();
-
-    let mut input1 = input.to_vec();
-    input1[unknown] = b'.';
-    let mut input2 = input.to_vec();
-    input2[unknown] = b'#';
-    return count_arrangements(&input1, groups) + count_arrangements(&input2, groups);
 }
 
-fn part_1(input: &str) -> u32 {
+fn part_1(input: &str) -> u64 {
     input
         .lines()
         .map(|line| {
@@ -85,7 +77,31 @@ fn part_1(input: &str) -> u32 {
                 .split(',')
                 .map(|n| n.parse::<u32>().unwrap())
                 .collect::<Vec<_>>();
-            count_arrangements(record.as_bytes(), &groups)
+
+            let mut cache = HashMap::new();
+            dfs(&mut cache, record.as_bytes(), &groups, 0, 0, 0)
+        })
+        .sum()
+}
+
+fn part_2(input: &str) -> u64 {
+    input
+        .lines()
+        .map(|line| {
+            let (record, groups) = line.split_once(' ').unwrap();
+            let mut groups = groups
+                .split(',')
+                .map(|n| n.parse::<u32>().unwrap())
+                .collect::<Vec<_>>();
+
+            let record = format!("{record}?{record}?{record}?{record}?{record}");
+            let initial_group = groups.clone();
+            for _ in 0..4 {
+                groups.append(&mut initial_group.clone());
+            }
+
+            let mut cache = HashMap::new();
+            dfs(&mut cache, record.as_bytes(), &groups, 0, 0, 0)
         })
         .sum()
 }
@@ -94,4 +110,5 @@ fn main() {
     let input = std::fs::read_to_string("input").unwrap();
 
     assert_eq!(part_1(&input), 7110);
+    assert_eq!(part_2(&input), 1566786613613);
 }
